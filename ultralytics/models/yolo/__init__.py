e370df730688e1249e833462cc5e254c1a4ebee9
UltralyticsAssistant <web@ultralytics.com>
2026-06-08 04:19:51 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/models/yolo/__init__.py
+++ ultralytics/models/yolo/__init__.py
@@ -1,6 +1,15 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-from ultralytics.models.yolo import classify, detect, obb, pose, segment, semantic, world, yoloe
+from ultralytics.models.yolo import (
+    classify,
+    detect,
+    obb,
+    pose,
+    segment,
+    semantic,
+    world,
+    yoloe,
+)
 
 from .model import YOLO, YOLOE, YOLOWorld
 
